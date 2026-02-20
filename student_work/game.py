3375ba37c5d41d4f6cd50525a66a4e3ddd74fd91
BraxtonCook1 <s1073737@student.knoxschools.org>
2026-02-20 15:37:43 +0000

    Game Data Complete

--- student_work/game.py
+++ student_work/game.py
@@ -1,8 +1,30 @@
 # Write your game here
+    import curses
 
 game_data = {
     # Store board dimensions, player/enemy positions, score, energy, collectibles, and icons
+
+    'width': 10,
+    'height': 10,
+    'knight': {"x": 0, "y": 0, "score": 0, "health": 100},
+    'dragon_pos': {"x": 10, "y": 10},
+    'princess': [
+        {"x": 10, "y": 1, "collected": False},
+    ],
+    'obstacles': [
+        {"x": 4, "y": 5},
+        {"x": 5, "y": 6},
+        {"x": 6, "y": 7}
+    ],
+
+    # Sticker Icons
+    'Knight': " ",
+    'Dragon': " ",
+    'Wall': " ",
+    'Princess': " ",
+    'empty': "  "
 }
+
 
 def draw_board(screen):
     # Print the board and all game elements using curses
